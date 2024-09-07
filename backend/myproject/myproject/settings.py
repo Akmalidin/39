08c67d3a16123be1e0ed0844313489c01c6616a6
Akbarali <akbarali@Pros-MacBook-Pro.local>
2024-09-07 15:21:17 +0600

    end

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q0d8_90dxq$ao*)i$!0o&r4%x_fo#h1gwtb*@9ud-fmk7u!i5z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -67,6 +67,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "https://cp.jino.ru",
+    "https://39ortomekteb.netlify.app"
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -90,17 +92,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # Database engine
+        'NAME': 'your_database_name',               # Your PostgreSQL database name
+        'USER': 'your_database_user',               # Your PostgreSQL username
+        'PASSWORD': 'your_database_password',       # Your PostgreSQL password
+        'HOST': 'localhost',                        # Or the address of your database server
+        'PORT': '5432',                             # Default port for PostgreSQL
+    }
+}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
